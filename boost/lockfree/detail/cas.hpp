af4474f915aceb4fc5555aa4f002813657b018a4
Tim Blechmann <tim@klingt.org>
2009-06-24 14:28:49 +0200

    cas: use boost::mpl to select cas by size

--- boost/lockfree/detail/cas.hpp
+++ boost/lockfree/detail/cas.hpp
@@ -15,6 +15,11 @@
 
 #include <boost/cstdint.hpp>
 
+#include <boost/mpl/map.hpp>
+#include <boost/mpl/at.hpp>
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/long.hpp>
+
 namespace boost
 {
 namespace lockfree
@@ -36,59 +41,89 @@ inline void memory_barrier()
 }
 
 template <typename C>
+struct atomic_cas_emulator
+{
+    static inline bool cas(volatile C * addr, C old, C nw)
+    {
+        static boost::detail::lightweight_mutex guard;
+        boost::detail::lightweight_mutex::scoped_lock lock(guard);
+
+        if (*addr == old)
+        {
+            *addr = nw;
+            return true;
+        }
+        else
+            return false;
+    }
+
+    typedef C cas_type;
+};
+
+
+template <typename C>
 inline bool atomic_cas_emulation(volatile C * addr, C old, C nw)
 {
-    static boost::detail::lightweight_mutex guard;
-    boost::detail::lightweight_mutex::scoped_lock lock(guard);
-
-    if (*addr == old)
-    {
-        *addr = nw;
-        return true;
-    }
-    else
-        return false;
+    return atomic_cas_emulator<C>::cas(addr, old, nw);
 }
 
 using boost::uint32_t;
 using boost::uint64_t;
 
-inline bool atomic_cas32(volatile uint32_t *  addr, uint32_t old, uint32_t nw)
+struct atomic_cas32
 {
+    static inline bool cas(volatile uint32_t *  addr, uint32_t old, uint32_t nw)
+    {
 #if defined(__GNUC__) && ( (__GNUC__ > 4) || ((__GNUC__ >= 4) && (__GNUC_MINOR__ >= 1)) ) || defined(__INTEL_COMPILER)
-    return __sync_bool_compare_and_swap(addr, old, nw);
+        return __sync_bool_compare_and_swap(addr, old, nw);
 #else
-    return boost::interprocess::detail::atomic_cas32(addr, old, nw) == old;
+        return boost::interprocess::detail::atomic_cas32(addr, old, nw) == old;
 #endif
-}
+    }
+    typedef uint32_t cas_type;
+};
 
-inline bool atomic_cas64(volatile uint64_t * addr, uint64_t old, uint64_t nw)
+struct atomic_cas64
 {
+    static inline bool cas(volatile uint64_t * addr, uint64_t old, uint64_t nw)
+    {
 #if defined(__GNUC__) && ( (__GNUC__ > 4) || ((__GNUC__ >= 4) && (__GNUC_MINOR__ >= 1)) ) || defined(__INTEL_COMPILER)
-    return __sync_bool_compare_and_swap(addr, old, nw);
+        return __sync_bool_compare_and_swap(addr, old, nw);
 #elif defined(_M_IX86)
-    return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(addr),
-                                      reinterpret_cast<LONG>(nw),
-                                      reinterpret_cast<LONG>(old)) == old;
+        return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(addr),
+                                          reinterpret_cast<LONG>(nw),
+                                          reinterpret_cast<LONG>(old)) == old;
 #elif defined(_M_X64)
-    return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(addr),
-                                      reinterpret_cast<LONG>(nw),
-                                      reinterpret_cast<LONG>(old)) == old;
+        return InterlockedCompareExchange(reinterpret_cast<volatile LONG*>(addr),
+                                          reinterpret_cast<LONG>(nw),
+                                          reinterpret_cast<LONG>(old)) == old;
 #else
 #warning ("blocking CAS emulation")
-    return atomic_cas_emulation(addr, old, nw);
+        return atomic_cas_emulation(addr, old, nw);
 #endif
-}
+    }
+
+    typedef uint64_t cas_type;
+};
 
 template <class C>
 inline bool atomic_cas(volatile C * addr, C old, C nw)
 {
-    if (sizeof(C) == 4)
-        return atomic_cas32((volatile uint32_t *)addr, (uint32_t)old, (uint32_t)nw);
-    else if (sizeof(C) == 8)
-        return atomic_cas64((volatile uint64_t *)addr, (uint64_t)old, (uint64_t)nw);
-    else
-        return atomic_cas_emulation(addr, old, nw);
+    using namespace boost::mpl;
+
+    typedef map2<pair<long_<4>, atomic_cas32>,
+        pair<long_<8>, atomic_cas64>
+        > cas_map;
+
+    typedef typename at<cas_map, long_<sizeof(C)> >::type atomic_cas_t;
+
+    typedef typename if_<has_key<cas_map, long_<sizeof(C)> >,
+        atomic_cas_t,
+        atomic_cas_emulator<C> >::type cas_t;
+
+    typedef typename cas_t::cas_type cas_value_t;
+
+    return cas_t::cas((volatile cas_value_t*)addr, (cas_value_t)old, (cas_value_t)nw);
 }
 
 
