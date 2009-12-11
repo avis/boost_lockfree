14cd62262f722f011dd4c00cbd3261df8c05b220
Tim Blechmann <tim@klingt.org>
2009-12-11 13:16:15 +0100

    lockfree: improved stack testsuite
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- libs/lockfree/test/stack_test.cpp
+++ libs/lockfree/test/stack_test.cpp
@@ -13,29 +13,33 @@ template <typename freelist_t>
 struct stack_tester
 {
     static const unsigned int buckets = 1<<10;
+    static const long node_count = 1000000;
+    static const int reader_threads = 4;
+    static const int writer_threads = 4;
+
     static_hashed_set<long, buckets> data;
     boost::array<std::set<long>, buckets> returned;
 
+    atomic_int push_count, pop_count;
+
     boost::lockfree::stack<long, freelist_t> stk;
+
+    stack_tester(void):
+        push_count(0), pop_count(0)
+    {}
 
     void add_items(void)
     {
-        unsigned long count = 100000;
-
-        for (unsigned long i = 0; i != count; ++i)
+        for (long i = 0; i != node_count; ++i)
         {
-            thread::yield();
             long id = generate_id<long>();
 
             bool inserted = data.insert(id);
-
             assert(inserted);
 
-
             while(stk.push(id) == false)
-            {
                 thread::yield();
-            }
+            ++push_count;
         }
     }
 
@@ -45,7 +49,6 @@ struct stack_tester
     {
         for (;;)
         {
-            thread::yield();
             long id;
 
             bool got = stk.pop(&id);
@@ -53,6 +56,7 @@ struct stack_tester
             {
                 bool erased = data.erase(id);
                 assert(erased);
+                ++pop_count;
             }
             else
                 if (not running)
@@ -71,10 +75,10 @@ struct stack_tester
 
         BOOST_REQUIRE(stk.empty());
 
-        for (int i = 0; i != 2; ++i)
+        for (int i = 0; i != reader_threads; ++i)
             reader.create_thread(boost::bind(&stack_tester::get_items, this));
 
-        for (int i = 0; i != 2; ++i)
+        for (int i = 0; i != writer_threads; ++i)
             writer.create_thread(boost::bind(&stack_tester::add_items, this));
 
         using namespace std;
@@ -91,6 +95,9 @@ struct stack_tester
 
         BOOST_REQUIRE_EQUAL(data.count_nodes(), 0);
         BOOST_REQUIRE(stk.empty());
+
+        BOOST_REQUIRE_EQUAL(push_count, pop_count);
+        BOOST_REQUIRE_EQUAL(push_count, writer_threads * node_count);
     }
 };
 
