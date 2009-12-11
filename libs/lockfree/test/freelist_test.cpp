78d33e2f852b1a408b131b3e9f25eb34f3f9a653
Tim Blechmann <tim@klingt.org>
2009-12-11 12:06:46 +0100

    lockfree: remove unused freelist class
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- libs/lockfree/test/freelist_test.cpp
+++ libs/lockfree/test/freelist_test.cpp
@@ -16,22 +16,6 @@ class dummy
     int foo[64];
 };
 
-boost::lockfree::freelist<dummy, 64> fl;
-
-BOOST_AUTO_TEST_CASE( freelist_test1 )
-{
-    std::vector<dummy*> nodes;
-
-    for (int i = 0; i != 128; ++i)
-        nodes.push_back(fl.allocate());
-
-    BOOST_FOREACH(dummy * d, nodes)
-        fl.deallocate(d);
-
-    for (int i = 0; i != 128; ++i)
-        nodes.push_back(fl.allocate());
-}
-
 boost::lockfree::caching_freelist<dummy> cfl;
 
 BOOST_AUTO_TEST_CASE( freelist_test2 )
