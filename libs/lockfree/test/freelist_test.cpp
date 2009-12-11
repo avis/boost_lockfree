e9fb7beeda1bdb20892e22e0adb249955031e391
Tim Blechmann <tim@klingt.org>
2009-12-11 12:35:52 +0100

    lockfree: add threaded freelist test
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- libs/lockfree/test/freelist_test.cpp
+++ libs/lockfree/test/freelist_test.cpp
@@ -1,4 +1,6 @@
 #include <boost/lockfree/detail/freelist.hpp>
+#include <boost/atomic.hpp>
+#include <boost/thread.hpp>
 
 #include <climits>
 #define BOOST_TEST_MODULE lockfree_tests
@@ -46,6 +48,61 @@ BOOST_AUTO_TEST_CASE( freelist_tests )
     run_test<boost::lockfree::caching_freelist<dummy> >();
     run_test<boost::lockfree::static_freelist<dummy> >();
 }
+
+template <typename freelist_type>
+struct freelist_tester
+{
+    static const int max_nodes = 1024;
+    static const int thread_count = 4;
+    static const int loops_per_thread = 1024;
+
+    boost::atomic_int free_nodes;
+    boost::thread_group threads;
+
+    freelist_type fl;
+
+    freelist_tester(void):
+        free_nodes(0), fl(max_nodes * thread_count)
+    {
+        for (int i = 0; i != thread_count; ++i)
+            threads.create_thread(boost::bind(&freelist_tester::run, this));
+        threads.join_all();
+    }
+
+    void run(void)
+    {
+        std::vector<dummy*> nodes;
+        nodes.reserve(max_nodes);
+
+        for (int i = 0; i != loops_per_thread; ++i) {
+            while (nodes.size() < max_nodes) {
+                dummy * node = fl.allocate();
+                if (node == NULL)
+                    break;
+                nodes.push_back(node);
+            }
+
+            while (!nodes.empty()) {
+                dummy * node = nodes.back();
+                assert(node);
+                nodes.pop_back();
+                fl.deallocate(node);
+            }
+        }
+        BOOST_REQUIRE(nodes.empty());
+    }
+};
+
+BOOST_AUTO_TEST_CASE( caching_freelist_test )
+{
+    freelist_tester<boost::lockfree::caching_freelist<dummy> > tester();
+}
+
+BOOST_AUTO_TEST_CASE( static_freelist_test )
+{
+    freelist_tester<boost::lockfree::static_freelist<dummy> > tester();
+}
+
 /* using namespace boost; */
 /* using namespace boost::mpl; */
 
