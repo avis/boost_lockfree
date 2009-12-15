3377bf26d85fecf4c6e011bf37efa1e5ebf49f4f
Helge Bahmann <hcb@chaoticmind.net>
2009-12-15 17:51:33 +0100

    Use boost/cstdint.hpp instead of directly including stdint.h

--- boost/atomic/detail/valid_integral_types.hpp
+++ boost/atomic/detail/valid_integral_types.hpp
@@ -1,7 +1,7 @@
 #ifndef BOOST_DETAIL_ATOMIC_VALID_INTEGRAL_TYPES_HPP
 #define BOOST_DETAIL_ATOMIC_VALID_INTEGRAL_TYPES_HPP
 
-#include <stdint.h>
+#include <boost/cstdint.hpp>
 
 namespace boost {
 namespace detail {
@@ -14,13 +14,15 @@ template<> struct is_integral_type<char> {typedef int test;};
 template<> struct is_integral_type<unsigned char> {typedef int test;};
 template<> struct is_integral_type<signed char> {typedef int test;};
 template<> struct is_integral_type<unsigned short> {typedef int test;};
-template<> struct is_integral_type<short> {typedef int test;};
+template<> struct is_integral_type<signed short> {typedef int test;};
 template<> struct is_integral_type<unsigned int> {typedef int test;};
-template<> struct is_integral_type<int> {typedef int test;};
+template<> struct is_integral_type<signed int> {typedef int test;};
 template<> struct is_integral_type<unsigned long> {typedef int test;};
 template<> struct is_integral_type<long> {typedef int test;};
-template<> struct is_integral_type<unsigned long long> {typedef int test;};
-template<> struct is_integral_type<long long> {typedef int test;};
+#ifndef BOOST_NO_INT64_T
+template<> struct is_integral_type<uint64_t> {typedef int test;};
+template<> struct is_integral_type<int64_t> {typedef int test;};
+#endif
 
 }
 }
